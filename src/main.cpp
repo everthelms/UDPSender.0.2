87c9385b74d28f9c099bdc9c2c6dce6ded5252cb
everthelms <evert.helms@yahoo.de>
2022-12-27 17:49:02 +0100

    Linux version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include<stdio.h>
-#include<winsock2.h>
 
+#if defined(_MSC_VER)
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#else
+
+#endif
+
+#include<stdio.h>
+
+#if defined(_MSC_VER)
+#include<winsock2.h>
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
+#else
+#include <string.h>
+#endif
+
+
+
+
+
+
 
 #include <string>
 #include <iostream>
@@ -13,43 +29,63 @@
 #define EOF (-1)
 
 
+#if defined(_MSC_VER)
 #pragma comment(lib, "RCLib.lib")
+#else
+
+#endif
 
 
 
-#ifdef __cplusplus    // If used by C++ code, 
+#if defined(_MSC_VER)
+#ifdef __cplusplus    // If used by C++ code,
 extern "C"
 {          // we need to export the C interface
 #endif
 
 	__declspec(dllexport) void __cdecl run(std::string appid, MessageHub* messageHub)
 	{
-		std::string myID = messageHub->createMessageQueue();     // send my data to robclub.com
-		messageHub->sendLine(appid, "confirmready?type=UdpSender.0.2&id=" + myID);   // achtung: es ist noch nicht sicher ob der wirklich registriert ist
+#else
+extern "C"
+{
+    void run(std::string appid, MessageHub* messageHub)
+    {
+#endif
 
-		SOCKET s;
-		WSADATA wsa;
+		std::string myID = messageHub->createMessageQueue();     // send my data to robclub.com
+		messageHub->sendLine(appid, "confirmready?type=UDPSender.0.2&id=" + myID);   // achtung: es ist noch nicht sicher ob der wirklich registriert ist
+
+
+		#if defined(_MSC_VER)
+        SOCKET s;
+        WSADATA wsa;
+        #else
+        int s;
+        #endif
+
+
+
 		struct sockaddr_in si_other;    // target socket
 		int slen = sizeof(si_other);
 
 
-		// *** Here starts the Messageloop ***  
+		// *** Here starts the Messageloop ***
 		while (true)
 		{
 			std::string nextmessagestring = messageHub->receiveLine(myID, 10);   // 10 us, d.h. nur wenn was da ist
 			RCMessage nextmessage = RCMessage(nextmessagestring);
 
-			
+
 
 			if (nextmessage.getCommand() == "init")
 			{
 				std::string ipstr = nextmessage.getParameterValue("ip");
 				std::string porstr = nextmessage.getParameterValue("port");
 				std::string confirmto = nextmessage.getParameterValue("confirmto");
-				
 
 
-				//Initialise winsock
+                #if defined(_MSC_VER)
+                //Initialise winsock
 				printf("\nInitialising Winsock...");
 				if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 				{
@@ -57,13 +93,25 @@ extern "C"
 					exit(EXIT_FAILURE);
 				}
 				printf("Initialised.\n");
+                #else
 
-				//Create a socket
+                #endif
+
+
+				#if defined(_MSC_VER)
+                //Create a socket
 				if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
 				{
 					printf("Could not create socket : %d", WSAGetLastError());
 				}
 				printf("Socket created.\n");
+                #else
+                if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
+                {
+                    printf("socket creation failed");
+                }
+                #endif
+
 				//std::cout << "Socket:" << s << "\n";
 				int targetport = stoi(porstr);
 
@@ -71,7 +119,13 @@ extern "C"
 				memset((char*)&si_other, 0, sizeof(si_other));
 				si_other.sin_family = AF_INET;
 				si_other.sin_port = htons(targetport);
-				si_other.sin_addr.S_un.S_addr = inet_addr(ipstr.c_str());
+
+
+				#if defined(_MSC_VER)
+                si_other.sin_addr.S_un.S_addr = inet_addr(ipstr.c_str());
+                #else
+                si_other.sin_addr.s_addr = inet_addr(ipstr.c_str());
+                #endif
 
 				messageHub->sendLine(confirmto, "confirm_init?id=" + myID);
 
@@ -79,7 +133,8 @@ extern "C"
 			if (nextmessage.getCommand() == "data_in")
 			{
 				//std::cout << myID << ":" << nextmessage.getMessage() << "\n";
-				//std::cout <<"Socket:" << s << "\n";
+
+
 
 				std::string pointerstr = nextmessage.getParameterValue("buffer");
 				std::string bufferlengthstr = nextmessage.getParameterValue("length");
@@ -87,11 +142,18 @@ extern "C"
 				char* sendbuf = (char*)pointer;
 				int sendbuflen = std::stoi(bufferlengthstr);
 
-				if (sendto(s, sendbuf, sendbuflen, 0, (struct sockaddr*)&si_other, slen) == SOCKET_ERROR)
+
+				#if defined(_MSC_VER)
+                if (sendto(s, sendbuf, sendbuflen, 0, (struct sockaddr*)&si_other, slen) == SOCKET_ERROR)
 				{
 					printf("sendto() failed with error code : %d", WSAGetLastError());
 				}
+                #else
+                sendto(s, sendbuf,sendbuflen,0,(struct sockaddr *)&si_other, slen);
+                #endif
+
 				free(sendbuf);
+
 			}
 
 			else if (nextmessage.getCommand() == "end")
@@ -102,7 +164,10 @@ extern "C"
 			}
 		}
 	}
-
+#if defined(_MSC_VER)
 #ifdef __cplusplus
+}
+#endif
+#else
 }
 #endif
